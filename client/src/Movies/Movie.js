29cf19f380355a9f97a9191a5add4e1bcc3e09f8
agohorel <gohorelalex@gmail.com>
2019-12-16 16:54:33 -0500

    break out MovieCard into separate component

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
-const Movie = props => {
+import MovieCard from "./MovieCard";
+
+const Movie = () => {
   const [movie, setMovie] = useState();
   const { id } = useParams();
 
@@ -23,32 +25,7 @@ const Movie = props => {
   //   addToSavedList(movie)
   // }
 
-  if (!movie) {
-    return <div>Loading movie information...</div>;
-  }
-
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
-  );
+  return <MovieCard movie={movie}></MovieCard>;
 };
 
 export default Movie;
